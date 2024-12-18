6b5cb1df1fc912b3c020ae7da20cdb180212a0e8
Dominic Sisneros <dsisnero@gmail.com>
2024-12-18 00:52:55 -0700

    refactor: Update filter method syntax in selection set spec

--- spec/unit/autocad/selection_set_spec.rb
+++ spec/unit/autocad/selection_set_spec.rb
@@ -11,16 +11,16 @@ describe Autocad::SelectionSet do
     describe '#filter' do
       it 'creates correct filter for circle or arc' do
         ss.filter do |f|
-          f.type('Circle').or(f.type('Arc'))
+          f.or(f.type('Circle'), f.type('Arc'))
         end
-        
+
         _(ss.filter_types).must_equal([-4, 0, 0, -4])
         _(ss.filter_values).must_equal(['<OR', 'Circle', 'Arc', 'OR>'])
       end
 
       it 'creates correct filter for text search' do
         ss.filter_text('*The*')
-        
+
         _(ss.filter_types).must_equal([-4, 0, 0, -4])
         _(ss.filter_values).must_equal(['<OR', 'TEXT', 'MTEXT', 'OR>'])
       end
@@ -29,7 +29,7 @@ describe Autocad::SelectionSet do
         ss.filter do |f|
           f.block_reference
         end
-        
+
         _(ss.filter_types).must_equal([0])
         _(ss.filter_values).must_equal(['INSERT'])
       end
@@ -37,10 +37,10 @@ describe Autocad::SelectionSet do
       it 'creates correct filter with not operator' do
         ss.filter do |f|
           f.not(
-            f.type('Circle').or(f.type('Arc'))
+            f.or(f.type('Circle'), f.type('Arc'))
           )
         end
-        
+
         _(ss.filter_types).must_equal([-4, -4, 0, 0, -4, -4])
         _(ss.filter_values).must_equal(['<NOT', '<OR', 'Circle', 'Arc', 'OR>', 'NOT>'])
       end
@@ -53,7 +53,7 @@ describe Autocad::SelectionSet do
             f.color(1)
           )
         end
-        
+
         _(ss.filter_types).must_equal([-4, 0, 8, 62, -4])
         _(ss.filter_values).must_equal(['<AND', 'Circle', '0', 1, 'AND>'])
       end
